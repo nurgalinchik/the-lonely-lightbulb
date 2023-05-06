341cf8f749a55b8f1ff379667ee38efc22d578cb
Nurgalinchik <75561812+nurgalinchik@users.noreply.github.com>
2023-05-06 17:35:43 +1000

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function pictureChange() {
     if (bulb.src.endsWith("images/on.svg")) {
         new Audio("audio/on.mp3").play();
         bulb.src = "images/off.svg";
+        text.style.color = "black";
         text.innerHTML = "<p>turn on the lightbulb to make it happy</p>";
     } 
 
@@ -32,15 +33,26 @@ function pictureChange() {
         counterDisplay.innerHTML = counter;
         if (counter === 21) {
             new Audio("audio/21.mp3").play();
+            text.style.color = "#008480";
         }
         else if (counter === 69) {
             new Audio("audio/69.mp3").play();
+            text.style.color = "#008480";;
         }
-        if (counter === 1000) {
-          new Audio("audio/1000.mp3").play();
+        else if (counter === 1000) {
+            new Audio("audio/1000.mp3").play();
+            text.style.color = "#008480";;
         }
         else if (counter === 420) {
-          new Audio("audio/420.mp3").play();
+            new Audio("audio/420.mp3").play();
+            text.style.color = "#008480";;
+        }
+        else if (counter === 107) {
+            new Audio("audio/107.mp3").play();
+            text.style.color = "#008480";;
+        }
+        else {
+            text.style.color = "black";
         }
     }
     bulb.classList.toggle("on");
